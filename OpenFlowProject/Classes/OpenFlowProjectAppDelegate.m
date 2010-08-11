8d52131d959fc67b41a74f1ff1800981ed20e64e
mcchang <aznm1ke31@gmail.com>
2010-08-11 11:17:06 -0700

    making status bar opaque black to match cover flow

--- OpenFlowProject/Classes/OpenFlowProjectAppDelegate.m
+++ OpenFlowProject/Classes/OpenFlowProjectAppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {    
-    
+    [application setStatusBarStyle:UIStatusBarStyleBlackOpaque];
     // Override point for customization after app launch    
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
